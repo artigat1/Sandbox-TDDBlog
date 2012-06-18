80bfae9653e1ee615f15a84a816ef3a9844ae327
Steve Doggett <steve.doggett@mobankgroup.com>
2012-06-18 09:44:28 +0100

    Bad hack to hard code connection string to the dataContext

--- TDDBlog.Data/DataContext.cs
+++ TDDBlog.Data/DataContext.cs
@@ -28,6 +28,8 @@ namespace TDDBlog.Data
             {
                 if (context == null) throw new ArgumentNullException("context");
 
+                context.Database.Connection.ConnectionString =
+                    "Server=abb310fd-3fb4-484e-8f8f-a07100a1d825.sqlserver.sequelizer.com;Database=dbabb310fd3fb4484e8f8fa07100a1d825;User ID=kmvwsqhdvlgeckmv;Password=p5adijLt7AwkPtisdV8cAZ5xH6XtJvgxGxua4PJoZF5qqgzEr5hszzwwaKCmtsSJ;";
                 if (Settings.Default.UpdateDatabaseStructure)
                     context.UpdateDatabaseStructure();
             }
